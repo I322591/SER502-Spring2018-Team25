3ce6deb2c453e8fb0ea4e5cb8b85937804ff86d1
Mahima Gupta <mahimagupta1195@gmail.com>
2018-04-20 22:34:39 -0700

    Created Intermediate Code File

---
+++ src/compiler/IntermediateCodeGenerator.java
@@ -0,0 +1,23 @@
+package compiler;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+
+
+
+
+public class IntermediateCodeGenerator extends GrammarBaseListener {
+	 	int count = 0;
+	    private LinkedHashMap<String, String> symbolTable = new LinkedHashMap<String, String>();
+	    public static List<String> intermediateFile = new ArrayList<String>();
+	   
+
+	   
+	  
+	    @Override 
+	    public void exitProgram(GrammarParser.ProgramContext context) {
+	        intermediateFile.add("terminate");
+	    }
+
+}
