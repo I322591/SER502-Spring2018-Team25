be2a1c5da1e373662cb8dc6e183bd3d6e85186a8
Aditya Kumar <adityakumar@Adityas-MacBook-Pro.local>
2018-04-20 23:16:42 -0700

    Changes to Intermediate Code Generator

--- src/compiler/IntermediateCodeGenerator.java
+++ src/compiler/IntermediateCodeGenerator.java
@@ -3,21 +3,17 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
-
-
-
-
 public class IntermediateCodeGenerator extends GrammarBaseListener {
 	 	int count = 0;
 	    private LinkedHashMap<String, String> symbolTable = new LinkedHashMap<String, String>();
-	    public static List<String> intermediateFile = new ArrayList<String>();
+	    public static List<String> intermediateFile = new ArrayList<String>();	   
 	   
-
-	   
-	  
+        /* Overriding the default implementation of exitProgram in GrammarBaseListener */
 	    @Override 
 	    public void exitProgram(GrammarParser.ProgramContext context) {
 	        intermediateFile.add("terminate");
 	    }
+    
+        
 
 }
