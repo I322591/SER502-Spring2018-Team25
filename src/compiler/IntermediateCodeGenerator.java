bd0083d019d7f8dd2858d9f3616099828f8b9446
nishitisawant <nnsawant@asu.edu>
2018-04-21 20:22:42 -0700

    overriding enterCheck function for generating intermediate code for check condition

--- src/compiler/IntermediateCodeGenerator.java
+++ src/compiler/IntermediateCodeGenerator.java
@@ -4,34 +4,50 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class IntermediateCodeGenerator extends RochGrammarBaseListener {
-	 	int count = 0;
-	    private LinkedHashMap<String, String> symbolTable = new LinkedHashMap<String, String>();
-	    public static List<String> intermediateFile = new ArrayList<String>();	   
+    int count = 0;
+    private LinkedHashMap<String, String> symbolTable = new LinkedHashMap<String, String>();
+    public static List<String> intermediateFile = new ArrayList<String>();
 	   
-        /* Overriding the default implementation of exitProgram in GrammarBaseListener */
-	    @Override
-		public void exitGrammarstart(RochGrammarParser.GrammarstartContext context) {
-	        intermediateFile.add("terminate");
-	    }
-    
-        /* Overriding the default implementation of exitDeclaration in GrammarBaseListener */
-        @Override
-		public void exitVariableDeclaration(RochGrammarParser.VariableDeclarationContext context) {
-	        int lineCount = context.getChildCount();
+    /* Overriding the default implementation of exitProgram in GrammarBaseListener */
+    @Override
+    public void exitGrammarstart(RochGrammarParser.GrammarstartContext context) {
+        intermediateFile.add("terminate");
+    }
 
-	        if (lineCount == 3) {
-	            if (context.getChild(1).getText().equals("=")) {
-	                if (symbolTable.containsKey(context.getChild(2).getText())) {
-	                    intermediateFile.add("PUSH " + context.getChild(0).getText() + " "
-	                            + symbolTable.get(context.getChild(2).getText()));
-	                } else {
+    /* Overriding the default implementation of exitDeclaration in GrammarBaseListener */
+    @Override
+    public void exitVariableDeclaration(RochGrammarParser.VariableDeclarationContext context) {
+        int lineCount = context.getChildCount();
+        if (lineCount == 3) {
+            if (context.getChild(1).getText().equals("=")) {
+                if (symbolTable.containsKey(context.getChild(2).getText())) {
+                    intermediateFile.add("PUSH " + context.getChild(0).getText() + " "
+                            + symbolTable.get(context.getChild(2).getText()));
+                } else {
 	                    intermediateFile.add("PUSH " + context.getChild(0).getText() + " " + context.getChild(2).getText());
-	                }
-	            }
-	        } else {
+                }
+            }
+        } else {
 	            System.out.println("ERROR: This assignment is not supported by our grammar.");
-	        }
-	    }
+        }
+    }
+    @Override
+    public void enterCheck(RochGrammarParser.CheckContext context) {
+        StringBuilder checkStatement = new StringBuilder();
+        String operator = context.getChild(1).getChild(1).getText();
+        intermediateFile.add("BOOL value");
+        checkStatement.append(OperatorRetriever.getOperator(operator));
+        checkStatement.append(" value ");
+        String leftVar = context.getChild(1).getChild(0).getText()
+        checkStatement.append(leftVar).append(" ");
+        String rightVar = context.getChild(1).getChild(0).getText()
+        checkStatement.append(rightVar);
+        intermediateFile.add(checkStatement.toString());
+        checkStatement.setLength(0);
+        checkStatement.append("CHECK ").append("value");
+        intermediateFile.add(statement.toString());
+    }
+
     
         /* TO DO - Written the code for check-then and until but realised that need to tweak our grammar a little to have a 
          cleaner code  */
